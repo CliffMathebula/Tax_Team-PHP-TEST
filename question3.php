0068e9a2c9cd578c12dcb5cdc05e8f50f0863371
Cliff Mathebula <57091876+CliffMathebula@users.noreply.github.com>
2026-02-26 23:39:12 +0200

    Update question3.php

--- question3.php
+++ question3.php
@@ -1,39 +1,24 @@
 <?php
 
 // QUESTION 3
-
-// consider the following array
 $person_array = array('Leanna', 'derek', 'Lisa', 'John', 'lancelot', 'Michael', 'norman', 'Lawrence of Arabia');
 
 /**
-  write a loop that will print out (on a new line) all names that
-  begin with L or l (both uppercase and lowercase) that are longer than 5 characters
+ * write a loop that will print out (on a new line) all names that
+ * begin with L or l (both uppercase and lowercase) that are longer than 5 characters
  */
 
-//SUPPLY YOUR ANSWER BELOW THIS COMMENT
+foreach ($person_array as $name) {
+    // 1. Get the first character
+    // Using $name[0] is shorthand for the first character of the string
+    $firstChar = strtolower($name[0]);
 
-$userinput = "l"; //user input 
-$input = preg_quote($userinput, '~');
-$result = preg_grep('~' . $input . '~', $data);
-$result = array_filter($data, function ($item) use ($input) {
-    if (stripos($item, $input) !== false) {
-        return true;
-    }
-    return false;
-});
-
-$firstCharacter = "";
-$string = '';
-
-foreach ($person_array as $key) {
-    $string =  $key;
-    //Get the first character using substr.
-    $firstCharacter = substr($string, 0, 1);
-
-    if (($firstCharacter == strtoupper($userinput)) || ($firstCharacter == strtolower($userinput))) {
-        //prints the array value if is greater than 5
-        if (strlen($string) > 5) {
-            echo $string . "&nbsp; &nbsp;";
-        }
+    // 2. Check conditions:
+    // - Does it start with 'l'?
+    // - Is the length greater than 5?
+    if ($firstChar === 'l' && strlen($name) > 5) {
+        // 3. Print on a new line
+        // Use <br> for browser display or PHP_EOL for terminal
+        echo $name . "<br>\n";
     }
 }
